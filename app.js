56217dc2de9b3d94fc3ddd415779c97c7acfc5b0
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-11 21:19:51 -0400

    finished introLab

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const app = express();
 // We create our own server named app
 // Express server handling requests and responses
 const app = express();
+app.use(express.static('public'));
 
 
 // our first Route
@@ -11,6 +12,24 @@ app.get('/', (request, response, next) => {
     response.send('<h1>Welcome Ironhacker. :)</h1>');
 });
 
+
+app.get('/cat', (request, response, next) => {
+    response.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Cat</title>
+        <link rel="stylesheet" href="/stylesheets/style.css" />
+      </head>
+      <body>
+        <h1>Cat</h1>
+        <p>This is my second route</p>
+        <img src="/images/cool-cat.jpg" />
+      </body>
+    </html>
+  `);
+});
 
 app.listen(3000, () => {
     console.log('My first app listening on port 3000!')
